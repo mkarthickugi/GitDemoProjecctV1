5a2e00a54f8704de1db13a7de3fe3ba0df42857d
LAKSHMI <LAKSHMI@YATHAVAN>
2021-09-20 10:52:14 +0530

    voval program added

---
+++ src/programs/VovalOrConsonent.java
@@ -0,0 +1,15 @@
+package programs;
+
+public class VovalOrConsonent {
+
+	public static void main(String[] args) {
+		
+		char c='b';
+		if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+			System.out.println("voval");
+		else
+			System.out.println("consonent");
+
+	}
+
+}
